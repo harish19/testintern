79a3d0863c7318f16cce4eb060feb3c252ae3f1b
harish <harish@mantralabsglobal.com>
2015-01-20 18:53:35 +0530

    functional test cases

--- tests/intern.js
+++ tests/intern.js
@@ -22,13 +22,13 @@ define({
 	// OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 	// capabilities options specified for an environment will be copied as-is
 	environments: [
-		{ browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
-		{ browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
-		{ browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
-		{ browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-		{ browserName: 'chrome', version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
-		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' },
-		{ browserName: 'safari', version: '7', platform: 'OS X 10.9' }
+		// { browserName: 'internet explorer', version: '11', platform: 'Windows 8.1' },
+		// { browserName: 'internet explorer', version: '10', platform: 'Windows 8' },
+		// { browserName: 'internet explorer', version: '9', platform: 'Windows 7' },
+		// { browserName: 'firefox', version: '28', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
+		// { browserName: 'chrome', version: '34', platform: [ 'OS X 10.9', 'Windows 7', 'Linux' ] },
+		{ browserName: 'safari', version: '6', platform: 'OS X 10.8' }
+		// { browserName: 'safari', version: '8.0.2', platform: 'OS X 10.10.1' }
 	],
 
 	// Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
@@ -36,6 +36,11 @@ define({
 
 	// Name of the tunnel class to use for WebDriver tests
 	tunnel: 'SauceLabsTunnel',
+
+	tunnelOptions: {
+        username: 'kjharish19',
+        accessKey: '74291124-c109-4593-85e9-640b055e3f8d'
+    },
 
 	// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
 	// loader
@@ -55,7 +60,7 @@ define({
 	suites: [ /* 'myPackage/tests/foo', 'myPackage/tests/bar' */ 'tests/hello'],
 
 	// Functional test suite(s) to run in each browser once non-functional tests are completed
-	functionalSuites: [ /* 'myPackage/tests/functional' */ ],
+	functionalSuites: [ /* 'myPackage/tests/functional' */ 'myPackage/tests/functional/index'],
 
 	// A regular expression matching URLs to files that should not be included in code coverage analysis
 	excludeInstrumentation: /^(?:tests|node_modules)\//
